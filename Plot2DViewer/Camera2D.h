911a4be6a90b4232be9c173f6b1c7bc2e3417f94
Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
2022-10-23 19:30:11 +0300

    new6

--- Plot2DViewer/Camera2D.h
+++ Plot2DViewer/Camera2D.h
@@ -36,10 +36,6 @@ private:
 public:
 	Camera2D(double L, double R, double B, double T) : L(L), R(R), B(B), T(T)
 	{
-		H = B + 1;
-		W = R - 1;
-		posX = 0.0;
-		posY = 0.0;
 	}
 	void Clear(HDC dc)
 	{
@@ -50,31 +46,22 @@ public:
 		RECT r;
 		GetClientRect(WindowFromDC(dc), &r);
 
-		W = r.right;
-		H = r.bottom;
+		W = r.right - 1;
+		H = r.bottom - 1;
 
 		B = (B + T) / 2 - (R - L) / 2 * H / W;
 		T = (B + T) / 2 + (R - L) / 2 * H / W;
-
-		// Данная процедура вызывается при изменении размеров окна
-		// В ней задаются значения величин W, H, а также настраиваются значения параметров L, R, B, T таким образом, чтобы обеспечить одинаковые масштабы по координатным осям
 	}
 	void MoveTo(double X, double Y)
 	{
 		posX = X;
 		posY = Y;
-		// Перемещение графического курсора (posX, posY)
-		// Обратите внимание, что мы действуем в мировых координатах
 	}
 	void LineTo(HDC dc, double X, double Y)
 	{
 		MoveToEx(dc, WorldToScreenX(posX), WorldToScreenY(posY), NULL);
 		::LineTo(dc, WorldToScreenX(X), WorldToScreenY(Y));
 		MoveTo(X, Y);
-		// Отрисовка линии из текущей позиции графического курсора в точку (X, Y) и его перемещение в эту точку
-		// Обратите внимание, что мы действуем в мировых координатах
-		// При отрисовке линии могут быть использованы WinApi функции
-		// ::MoveToEx(dc, Xs, Ys, nullptr) и ::LineTo(dc, Xs, Ys)
 	}
 	void Axes(HDC dc)// Отрисовка координатных осей
 	{
